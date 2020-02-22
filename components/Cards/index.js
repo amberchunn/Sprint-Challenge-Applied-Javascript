6c69c5f7285ea1c9fe230e50d7162976527e2d27
Amber Chunn <a.77@me.com>
2020-02-21 18:54:51 -0800

    Added Axios Call to Cards Page.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,31 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+// Axios Get Request from News Source
+axios
+	.get('https://lambda-times-backend.herokuapp.com/articles')
+	.then(response => {
+		let data = response.data;
+		// console.log('Success. You mission succedded');
+		// console.log(data.articles);
+
+		data.articles.forEach(item => {
+			console.log(item);
+		});
+
+		// Set Up Car Structure
+		let card = document.createElement('div').setAttribute('class', 'card');
+		let headline = document.createElement('div').setAttribute('class', 'headline');
+		let author = document.createElement('div').setAttribute('class', 'author');
+		let imgContainer = document
+			.createElement('div')
+			.setAttribute('class', 'img-container');
+		let image = document.createElement('img').setAttribute('src', '#');
+		let byline = document.createElement('span');
+
+		// Get Card Data
+	})
+	.catch(error => {
+		console.log('Error:', error);
+	});
